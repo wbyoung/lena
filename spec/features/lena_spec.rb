3fd5435300b584ea70d171371c767bad09aecf6b
Whitney Young <wbyoung@fadingred.com>
2013-11-28 08:59:10 -0300

    Better testing setup.

--- spec/features/lena_spec.rb
+++ spec/features/lena_spec.rb
@@ -1,30 +1,50 @@
 require 'spec_helper'
 
-describe 'generating errors', js: true do
-  it 'includes data attributes' do
-    visit root_path
-    expect(page.body).to include('data-lena-js-url')
-    expect(page.body).to include('data-lena-destination')
+describe 'lena test app' do
+
+  context 'with default capture' do
+    it 'throws exceptions' do
+      expect { visit lena.js_submission_path }.to raise_error(Lena::JavaScriptError)
+    end
   end
 
-  it 'reports errors for log' do
-    expect { visit log_path }.to change { Lena.events.length }.by(1)
-    event = Lena.events[-1]
-    expect(event[:message]).to eq("Simple error log")
-    expect(event[:stacktrace]).to eq("unsupported")
-  end
+  context 'with custom capture', js: true do
+    before do
+      @javascript_handler = Lena::Engine.config.javascript_handler
+      @events = []
+      handler = Proc.new { |params| @events << params }
+      Lena.setup { |config| config.javascript_handler = handler }
+    end
 
-  it 'reports errors for exceptions' do
-    expect { visit throw_path }.to change { Lena.events.length }.by(1)
-    event = Lena.events[-1]
-    expect(event[:message]).to eq("Error: Simple error throw\nResource: undefined:0")
-    expect(event[:stacktrace]).to be_nil
-  end
+    after do
+      Lena::Engine.config.javascript_handler = @javascript_handler
+    end
 
-  it 'reports errors when nested, but cannot provide stack trace' do
-    expect { visit throw_callstack_path }.to change { Lena.events.length }.by(1)
-    event = Lena.events[-1]
-    expect(event[:message]).to eq("ReferenceError: Can't find variable: undefinedFunctionCall\nResource: undefined:0")
-    expect(event[:stacktrace]).to be_nil
+    it 'includes data attributes' do
+      visit root_path
+      expect(page.body).to include('data-lena-js-url')
+      expect(page.body).to include('data-lena-destination')
+    end
+
+    it 'reports errors for log' do
+      expect { visit log_path }.to change { @events.length }.by(1)
+      event = @events[-1]
+      expect(event[:message]).to eq("Simple error log")
+      expect(event[:stacktrace]).to eq("unsupported")
+    end
+
+    it 'reports errors for exceptions' do
+      expect { visit throw_path }.to change { @events.length }.by(1)
+      event = @events[-1]
+      expect(event[:message]).to eq("Error: Simple error throw\nResource: undefined:0")
+      expect(event[:stacktrace]).to be_nil
+    end
+
+    it 'reports errors when nested, but cannot provide stack trace' do
+      expect { visit throw_callstack_path }.to change { @events.length }.by(1)
+      event = @events[-1]
+      expect(event[:message]).to eq("ReferenceError: Can't find variable: undefinedFunctionCall\nResource: undefined:0")
+      expect(event[:stacktrace]).to be_nil
+    end
   end
 end
